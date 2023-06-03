b544fdcc86a191202fbd7c5757b13c4dfb3e4f80
Richard Bachmann <102971493+rb-uf@users.noreply.github.com>
2023-06-03 14:12:42 -0400

    misc: -

---
+++ multicast-dev/parsec-se-medium.sh
@@ -0,0 +1,21 @@
+../build/X86/gem5.debug \
+    --stats-file=parsec-se-stats.txt \
+    --debug-flags='GarnetMulticast' \
+    ../configs/deprecated/example/se.py \
+    --ruby \
+    --network=garnet \
+    --cpu-type=TimingSimpleCPU \
+    --caches \
+    --l1d_size=16kB \
+    --l1i_size=16kB \
+    --l2cache \
+    --num-l2cache=16 \
+    --num-cpus=16 \
+    --num-dirs=16 \
+    --topology=Mesh_XY \
+    --mesh-rows=4 \
+    --cpu-clock=2GHz \
+    --mem-size=4GB \
+    --cmd=../../parsec-benchmark/pkgs/apps/blackscholes/inst/amd64-linux.gcc/bin/blackscholes \
+    --options="8 ../../parsec-benchmark/pkgs/apps/blackscholes/run/in_4.txt prices.txt" \
+    --multicast \
